ffdda0699af546d7b555a6c79ffb033e6eaa1829
neilbadal <zcabbad@ucl.ac.uk>
2022-02-04 15:40:24 +0000

    added pre-processing

--- chatbot_retrieval/chatbot2.py
+++ chatbot_retrieval/chatbot2.py
@@ -1,17 +1,24 @@
-import nltk
-nltk.download('punkt')
+import nltk as N
+import string
 import re
+from nltk.corpus import stopwords
 
 def cleanInput(user_input):
   user_input = user_input.upper()
+  user_input.translate(string.punctuation)
   user_input = re.sub("[^A-Z0-9\s]", "", user_input)
   return user_input
 
-question = input("Please enter the question: ")
-cleanedQuestion = cleanInput(question)
-tokenisedQuestion = nltk.word_tokenize(cleanedQuestion)
-print(tokenisedQuestion)
-# Loop through tokens and see if it matches the existing sections
-# Group by 2/3 - using nltk
-# Loop through tokens and see if it matches the existing sections
-# If all the sections matches then search query
+def remove_stopWords_tokenize(q):
+  exceptions = {"how"}
+  stopWords = set(stopwords.words('english'))
+  stop_words = stopWords.difference(exceptions)
+  word_tokens = N.word_tokenize(q)
+  filtered = [w for w in word_tokens if not w.lower() in stop_words]
+  return filtered
+
+if __name__=="__main__":
+  q = input("Please enter the question: ")
+  cleanedQ = cleanInput(q)
+  cleanedQ = remove_stopWords_tokenize(cleanedQ)
+  print(cleanedQ)

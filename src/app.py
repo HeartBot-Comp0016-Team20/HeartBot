b2be996cfdb86789c90d7b728649e66e07b54b8b
zcabiva <ivan.varbanov.20@ucl.ac.uk>
2022-02-28 15:16:22 +0000

    website update

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from Retrieval import retriever
 from FAQS import converse
 from FAQS import aux_functions
 from flask import Flask, render_template, request
+import pandas as pd
 
 def run_chatbot(user_input):
     pairs = aux_functions.create_pairs()
@@ -13,7 +14,11 @@ def run_chatbot(user_input):
         return str(output[1])
     # Result is NOT found in FAQ so try querying the database
     else:
-        return str(retriever.run(output[1]))
+        res = retriever.run(output[1])
+        if isinstance(res,str) and res == "I don't understand\n":
+            return "No data found for your question\n"
+        else:
+            return res.to_html()
 
 
 app = Flask(__name__)
